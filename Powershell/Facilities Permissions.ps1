0da1b218e94aae42f482c0652f2ae5427a7be549
Ryan Lowell <ryan.lowell@outlook.com>
2021-01-23 15:54:57 -0500

    Removing customer specific info

--- Powershell/Facilities Permissions.ps1
+++
@@ -1,6 +0,0 @@
-﻿Remove-MailboxPermission -Identity "servicewpb@facilitiespro-sweep.com" -User "tcorrea@facilitiespro-sweep.com" -AccessRights FullAccess -InheritanceType All
-Remove-MailboxPermission -Identity "tcorrea@facilitiespro-sweep.com" -User "servicewpb@facilitiespro-sweep.com" -AccessRights FullAccess -InheritanceType All
-Remove-MailboxPermission -Identity "adminwpb@facilitiespro-sweep.com" -User "tcorrea@facilitiespro-sweep.com" -AccessRights FullAccess -InheritanceType All
-Remove-MailboxPermission -Identity "tcorrea@facilitiespro-sweep.com" -User "adminwpb@facilitiespro-sweep.com" -AccessRights FullAccess -InheritanceType All
-Remove-MailboxPermission -Identity "adminwpb@facilitiespro-sweep.com" -User "servicewpb@facilitiespro-sweep.com" -AccessRights FullAccess -InheritanceType All
-Remove-MailboxPermission -Identity "servicewpb@facilitiespro-sweep.com" -User "adminwpb@facilitiespro-sweep.com" -AccessRights FullAccess -InheritanceType All
