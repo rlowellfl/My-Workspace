c2eb65a1705e83eceb7d200156e23208a43c9e89
Ryan Lowell <ryan.lowell@outlook.com>
2021-01-28 09:19:01 -0500

    Refactored URL variables into class objects

--- Python/happybaby.py
+++ Python/happybaby.py
@@ -1,58 +1,33 @@
-#!/usr/bin/env python3
-# Script to check the 
-
+#Import modules
 import requests
 import lxml
 from bs4 import BeautifulSoup
 
-# set the url to the following
-url = "https://happybabycarriers.com/products/happy-baby-carrier/?variant=32356402397271"
+#define the carrierColor class
+class carrierColor:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+Umber = carrierColor("Raw Umber", "https://happybabycarriers.com/products/happy-baby-carrier/?variant=32356402397271")
+Cider = carrierColor("Cider", "https://happybabycarriers.com/products/happy-baby-carrier/?variant=32356401381463")
+Juniper = carrierColor("Juniper", "https://happybabycarriers.com/products/happy-baby-carrier/?variant=32356401512535")
+
+colors = Umber, Cider, Juniper
+
 # set the headers like we are a browser,
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-# download the homepage
-response = requests.get(url, headers=headers)
-# parse the downloaded homepage and grab all text, then,
-soup = BeautifulSoup(response.text, "lxml")
 
-# if the number of times the word "Add to cart" occurs on the page is less than 1,
-if str(soup).find("ADD TO CART") > 0:
-    print('Available in Raw Umber')
-# but if the word "Add to cart" occurs any other number of times,
-else:
-    print('Out of stock in Raw Umber')
+for carrierColor in colors:
+    # download the homepage
+    response = requests.get(carrierColor.url, headers=headers)
+    # parse the downloaded homepage and grab all text, then,
+    soup = BeautifulSoup(response.text, "lxml")
+    # if the number of times the word "Add to cart" occurs on the page is less than 1,
+    if str(soup).find("ADD TO CART") > 0:
+        print('Available in ' + carrierColor.name)
+    # but if the word "Add to cart" occurs any other number of times,
+    else:
+        print('Out of stock in ' + carrierColor.name)
 
-# set the url to the following
-url = "https://happybabycarriers.com/products/happy-baby-carrier/?variant=32356401512535"
-# set the headers like we are a browser,
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-# download the homepage
-response = requests.get(url, headers=headers)
-# parse the downloaded homepage and grab all text, then,
-soup = BeautifulSoup(response.text, "lxml")
-
-# if the number of times the word "Add to cart" occurs on the page is less than 1,
-if str(soup).find("ADD TO CART") > 0:
-    print('Available in Juniper')
-# but if the word "Add to cart" occurs any other number of times,
-else:
-    print('Out of stock in Juniper')
-
-# set the url to the following
-url = "https://happybabycarriers.com/products/happy-baby-carrier/?variant=32356401381463"
-# set the headers like we are a browser,
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-# download the homepage
-response = requests.get(url, headers=headers)
-# parse the downloaded homepage and grab all text, then,
-soup = BeautifulSoup(response.text, "lxml")
-
-# if the number of times the word "Add to cart" occurs on the page is less than 1,
-if str(soup).find("ADD TO CART") > 0:
-    print('Available in Cider')
-# but if the word "Add to cart" occurs any other number of times,
-else:
-    print('Out of stock in Cider')
-
-#add multiple colors as objects within a JSON object and loop through each
-#add results to a JSON object
 #output results to the JSON object so Logic Apps can parse it
