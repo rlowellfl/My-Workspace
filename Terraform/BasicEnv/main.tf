bde85d060af2c3c9d56123e64e1c2aba515b4f01
Ryan Lowell <ryan.lowell@outlook.com>
2021-03-29 08:43:39 -0400

    Initial terraform commit

---
+++ Terraform/BasicEnv/main.tf
@@ -0,0 +1,74 @@
+#Provide providers
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.53.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "resourceGroup" {
+  name     = var.rgName
+  location = var.regionName
+}
+
+# Create the virtual network
+resource "azurerm_virtual_network" "vNet" {
+  name                = var.vnetName
+  address_space       = var.vnetRange
+  location            = azurerm_resource_group.resourceGroup.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+}
+
+# Create a subnet within the virtual network
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.vnetSubnetName
+  resource_group_name  = azurerm_resource_group.resourceGroup.name
+  virtual_network_name = azurerm_virtual_network.vNet.name
+  address_prefixes     = var.vnetSubnetRange
+}
+
+# Create a virtual NIC and attach to the vnet/subnet
+resource "azurerm_network_interface" "vmNIC" {
+  name                = var.vmNicName
+  location            = azurerm_resource_group.resourceGroup.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Create an Azure virtual machine attached to the above resources
+resource "azurerm_windows_virtual_machine" "vm1" {
+  name                = var.vmName
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  location            = azurerm_resource_group.resourceGroup.location
+  size                = var.vmSize
+  admin_username      = "adminuser"
+  admin_password      = "tdSw5SA7fGpM"
+  network_interface_ids = [
+    azurerm_network_interface.vmNIC.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
---
+++ Terraform/BasicEnv/variables.tf
@@ -0,0 +1,49 @@
+variable "rgName" {
+  type = string
+  description = "The name of the new resource group"
+}
+
+variable "regionName" {
+  type  = string
+  description = "Provide a region to deploy the new environment"
+  default = "EastUS2"
+}
+
+variable "vnetName" {
+  type  = string
+  description = "Provide a name for the new VNet"
+}
+
+variable "vnetRange" {
+  type  = list
+  description = "Provide an address range in CIDR notation. Default 10.0.0.0/16"
+  default = ["10.0.0.0/16"]
+}
+
+variable "vnetSubnetName" {
+  type  = string
+  description = "Provide a name for the new subnet. Default Subnet1"
+  default = "Subnet1"
+}
+
+variable "vnetSubnetRange" {
+  type  = list
+  description = "Provide an address range in CIDR notation. Default 10.0.0.0/24"
+  default = ["10.0.0.0/24"]
+}
+
+variable "vmName" {
+  type  = string
+  description = "Provide a name for the new VM"
+}
+
+variable "vmSize" {
+  type  = string
+  description = "Provide a size for the VM. Default Standard_B1ms"
+  default = "Standard_B1ms"
+}
+
+variable "vmNicName" {
+  type  = string
+  description = "Provide a name for the VM's NIC"
+}
